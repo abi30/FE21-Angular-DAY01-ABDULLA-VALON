f5b2eb34eeadcaece54fbcaa130a5879ea863be6
Abdulla Al Rakib <36118180+abi30@users.noreply.github.com>
2021-03-22 16:04:21 +0100

    hello done

--- src/app/readmore/readmore.component.ts
+++ src/app/readmore/readmore.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-readmore',
+  selector: 'readmore',
   templateUrl: './readmore.component.html',
   styleUrls: ['./readmore.component.css']
 })
