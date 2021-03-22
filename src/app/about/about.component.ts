ff2cc04be32e0e78ddcfeb4b396df2228d15d443
Abdulla Al Rakib <36118180+abi30@users.noreply.github.com>
2021-03-22 13:20:08 +0100

    going

---
+++ src/app/about/about.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+
+
+  heroHeading: string;
+
+  heroText: string;
+
+  heroBtnText : string;
+
+  heroBtnUrl:   string ;
+
+  constructor() { 
+
+    this.heroHeading =  "MyFirstAngularApp" ;
+
+    this.heroText =  "The demand, to teach coding as a modern language in schools is also voiced on a state-level both here and abroad. The realization of this goal, to have a high-quality and standardized IT-education in the curriculum, is still a long way off though." ;
+
+    this.heroBtnText =  "Learn more" ;
+
+    this.heroBtnUrl =  "https://codefactory.wien"  ;
+
+  }
+
+  ngOnInit(): void {
+  }
+
+}
